1db1e629bd3227730cd48caa18e0c590d0b1137c
pospi <pospi@spadgos.com>
2019-11-27 20:50:32 +1000

    update yarn lockfile

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,9 +1,39 @@
 const path = require('path')
 
+const sveltePreprocess = require('svelte-preprocess')
+
 module.exports = ({ config, mode }) => {
+
+  const svelteLoaderPath = require.resolve('svelte-loader')
+
+  // :NOTE: only visible when run with `--debug-webpack` CLI flag
+  require('util').inspect(config, { depth: null, colors: true })
+
+  // enable sourcemaps
   if (mode === 'DEVELOPMENT') {
     config.devtool = 'inline-source-map'
   }
+
+  config.module.rules.forEach(rule => {
+    // inject preprocessor plugin into Svelte
+    if (rule.loader === svelteLoaderPath) {
+      rule.options = {
+        preprocess: [
+          sveltePreprocess({
+            postcss: true,
+            // :TODO: has issues with Svelte `3.15.0`, subsequent versions may fix
+            // typescript: {
+            //   tsconfigDirectory: path.resolve(__dirname, '../'),
+            //   transpileOnly: false,
+            // },
+            stylus: false,
+            scss: false,
+            pug: false,
+          }),
+        ],
+      }
+    }
+  })
 
   config.module.rules.push(
     // load in any raw CSS files using PostCSS

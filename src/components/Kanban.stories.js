ce77c09318a8de6881a0a01b44a905a28b65281e
pospi <pospi@spadgos.com>
2019-11-27 15:02:00 +1000

    add markdown component notes

--- src/components/Kanban.stories.js
+++ src/components/Kanban.stories.js
@@ -1,5 +1,6 @@
 import { storiesOf } from '@storybook/svelte'
 import Kanban from './Kanban.svelte'
+import markdownNotes from './Kanban.stories.md'
 
 storiesOf('Kanban|Whole board', module)
   .add(
@@ -9,4 +10,5 @@ storiesOf('Kanban|Whole board', module)
       props: {
       },
     }),
+    { notes: { markdown: markdownNotes } },
   )
---
+++ src/components/Kanban.stories.md
@@ -0,0 +1,1 @@
+This is a port of [@ivanminutillo's original Kanban component](https://github.com/ivanminutillo/kanban-webcomponent/) to Svelte and `vf-graphql`.

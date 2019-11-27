125f9ad623ccbf828276c28bb44dd3b1fb9643d4
pospi <pospi@spadgos.com>
2019-11-27 16:10:29 +1000

    more Kanban stories

--- src/components/Kanban.stories.js
+++ src/components/Kanban.stories.js
@@ -3,11 +3,40 @@ import Kanban from './Kanban.svelte'
 import markdownNotes from './Kanban.stories.md'
 
 storiesOf('Kanban|Whole board', module)
+
   .add(
     'empty state',
     () => ({
       Component: Kanban,
       props: {
+      },
+    }),
+    { notes: { markdown: markdownNotes } },
+  )
+
+  .add(
+    'with tasks',
+    () => ({
+      Component: Kanban,
+      props: {
+        bins: [{
+          title: 'first batch of tasks',
+          note: 'doing stuff',
+          due: new Date(),
+          cards: [{
+            note: 'a task that needs doing',
+            due: new Date(),
+            members: [
+              { image: 'https://randomuser.me/api/portraits/women/28.jpg' },
+              { image: 'https://randomuser.me/api/portraits/women/23.jpg' },
+            ],
+          }],
+          outputs: [{
+            resourceClassifiedAs: {
+              name: 'an output resource',
+            },
+          }],
+        }],
       },
     }),
     { notes: { markdown: markdownNotes } },

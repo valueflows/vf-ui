f63ca2ede937a72450a066b3296c27afc8dbbe65
pospi <pospi@spadgos.com>
2019-12-09 18:41:47 +1000

    add specifications storybook addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,5 @@
 // import '@storybook/addon-storysource/register'
+import 'storybook-addon-specifications/register'
 import '@storybook/addon-actions/register'
 import '@storybook/addon-notes/register'
 import '@storybook/addon-viewport/register'

f8dd09f84e5259840c0a558bb6c8ae805e660ac7
pospi <pospi@spadgos.com>
2019-12-10 15:20:18 +1000

    integrate tests with toplevel Kanban component storybook

--- src/components/Kanban.stories.js
+++ src/components/Kanban.stories.js
@@ -1,16 +1,26 @@
-import { storiesOf } from '@storybook/svelte'
+import { storiesOf, specs, describe, it } from '!/facade'
+import { render } from '@testing-library/svelte'
+import expect from 'expect'
+
 import Kanban from './Kanban.svelte'
 import markdownNotes from './Kanban.stories.md'
 
 storiesOf('Kanban|Whole board', module)
 
-  .add(
-    'empty state',
-    () => ({
-      Component: Kanban,
-      props: {
-      },
-    }),
+  .add('empty state',
+    () => {
+      specs(() => describe('empty state', function () {
+        it('Should render OK without any props provided', function () {
+          expect(render(Kanban)).toBeTruthy()
+        })
+      }))
+
+      return {
+        Component: Kanban,
+        props: {
+        },
+      }
+    },
     { notes: { markdown: markdownNotes } },
   )
 

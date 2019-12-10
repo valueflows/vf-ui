be9f40ebf0c18ba1496a3c09f1e138d2a9adba78
pospi <pospi@spadgos.com>
2019-12-10 14:34:36 +1000

    more storybook / jest wiring

--- .storybook/facade.js
+++ .storybook/facade.js
@@ -1,5 +1,21 @@
-import {storiesOf as storiesOfReal, action as actionReal, linkTo as linkToReal} from "@storybook/svelte"
-import { specs as specsReal, describe as describeReal, it as itReal } from 'storybook-addon-specifications'
+import {
+    storiesOf as storiesOfReal,
+    action as actionReal,
+    linkTo as linkToReal
+} from '@storybook/svelte';
+
+import {
+    specs as specsReal,
+    describe as describeReal,
+    it as itReal,
+    beforeEach as beforeEachReal,
+    before as beforeReal,
+    after as afterReal,
+    afterEach as afterEachReal,
+    xit as xitReal,
+    fit as fitReal,
+    xdescribe as xdescribeReal
+} from 'storybook-addon-specifications';
 
 export const storiesOf = storiesOfReal;
 export const action = actionReal;
@@ -7,3 +23,12 @@ export const linkTo = linkToReal;
 export const specs = specsReal;
 export const describe = describeReal;
 export const it = itReal;
+
+export const beforeEach = beforeEachReal;
+export const afterEach = afterEachReal;
+export const before = beforeReal;
+export const after = afterReal;
+
+export const xit = xitReal;
+export const fit = fitReal;
+export const xdescribe = xdescribeReal;

76649c91ef46a4d59b80a6bbad6d6670cda51f6a
pospi <pospi@spadgos.com>
2019-12-10 13:57:47 +1000

    setup Jest and Babel configs

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  testMatch: [
+    '**/?(*.)+(stories|spec|test).[tj]s?(x)',
+  ],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.svelte$': ['svelte-jester', { preprocess: true }],
+  },
+  moduleFileExtensions: ['js', 'svelte'],
+  bail: false,
+  verbose: false,
+  automock: false,
+  setupFiles: [
+    path.resolve(__dirname, './jest.setup.js'),
+  ],
+  setupFilesAfterEnv: [
+    // '@testing-library/svelte/cleanup-after-each',
+    '@testing-library/jest-dom/extend-expect',
+  ],
+}

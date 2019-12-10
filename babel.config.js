8579194c4fb64aafe2d648f9dc0cefee384be23c
pospi <pospi@spadgos.com>
2019-12-10 15:03:57 +1000

    wire up root import plugin for loading framework CSS and storybook config files used in tests

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,24 @@
+const path = require('path')
+
 module.exports = {
   presets: ['@babel/preset-env'],
-  // plugins: [
+  plugins: [
+    ['babel-plugin-root-import', {
+      paths: [
+        {
+          rootPathSuffix: path.resolve(__dirname, './src'),
+          rootPathPrefix: '~/',
+        },
+        {
+          rootPathSuffix: path.resolve(__dirname, './.storybook'),
+          rootPathPrefix: '!/',
+        },
+      ],
+    }],
   //   ["@babel/plugin-transform-runtime",
   //     {
   //       "regenerator": true
   //     }
   //   ]
-  // ],
+  ],
 }

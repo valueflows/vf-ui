3445f8c41ce483ef170f4e7212595d1b9fdc8d59
pospi <pospi@spadgos.com>
2019-12-16 17:47:49 +1000

    add note about svelte.config wiring

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,10 @@
 // :NOTE: This file is picked up by some editor tooling and will determine how syntax is
 //        handled in different Svelte template blocks.
+//
+//        It is also wired up manually to other config files in this repo so that it remains
+//        a canonical source of truth.
 
-const path = require('path')
+// const path = require('path')
 const sveltePreprocess = require('svelte-preprocess')
 
 module.exports = {
